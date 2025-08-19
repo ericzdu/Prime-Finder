8bb19c7c1befcaf1a1eee812631828a263b7bd74
ericzdu <ericdu.awesome@gmail.com>
2025-08-19 11:06:43 -0700

    initial linked list

--- prime_finder/prime_finder/main.c
+++ prime_finder/prime_finder/main.c
@@ -10,6 +10,17 @@ typedef struct Node {
 	struct Node* next;
 }Node;
 
+//if input is prime return prime
+int find_prime(int prime, int* count) {
+	for (int i = 2; i < prime / 2; ++i) {
+		if (prime % i == 0) {
+			return 0;
+		}
+	}
+
+	++ * count;
+	return prime;
+}
 
 //divide by all prime numbers before
 //store linked list of all previous primes
@@ -20,18 +31,32 @@ int main(int argc, char* argv[]) {
 	time_t start_time = time(NULL);
 	time_t current_time;
 	Node* head = malloc(sizeof(Node));
+	if (head == NULL) {
+		exit(1); 
+	}
 	Node* curr = head;
 	int i = 0;
 	int prime = 2;
-	int found;
 	int count = 0;
+	int found = 0;
+
 
 	do {
 		//15 second timer
 		current_time = time(NULL);
 		if (prime == 2 || prime % 2 == 1) {
 			found = find_prime(prime, &count);
-			curr->number = found;
+			if (found) {
+				curr->number = found;
+				Node* next = malloc(sizeof(Node));
+				if (next != NULL) {
+					curr->next = next;
+					curr = next;
+				}
+				else {
+					exit(1); 
+				}
+			}
 			//allocate space for next node and store it in current
 		}
 
@@ -46,18 +71,14 @@ int main(int argc, char* argv[]) {
 
 	printf("highest prime found: %d\n", found);
 	printf("number of primes found: %d\n", count);
-	return 0;
-}
 
-//if input is prime return prime
-int find_prime(int prime, int* count) {
-	for (int i = 2; i < prime / 2; ++i) {
-		if (prime % i == 0) {
-			return 0;
-		}
+	//print and free all primes: 
+	curr = head; 
+	while (curr != NULL) {
+		printf("%d, ", curr->number);
+		curr = curr->next;
 	}
 
-	++ * count;
-	return prime;
+	//free everything
+	return 0;
 }
-
